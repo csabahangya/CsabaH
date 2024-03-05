fcaf04199af40a775976fb60d582f9188ce39d9f
Csaba Hangya <hangyacsaba@hotmail.com>
2024-03-05 21:04:59 +0100

    FirstCommit

---
+++ Assets/nelegyenszoköz.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+class nelegyenszoköz : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    void Start()
+    {
+        Debug.Log("Hello Unity!");
+        Debug.Log("I'm " + name);
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.up * speed * Time.deltaTime;
+    }
+}   
+ 
